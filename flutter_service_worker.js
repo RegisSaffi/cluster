a82c513dd1ad52b7ae75947f114fa4a78ee3f306
GitHub Action <action@github.com>
2025-12-17 22:15:39 +0000

    Deploy from GitHub Action

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -9,8 +9,8 @@ const RESOURCES = {"flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "4f1477fe7948b4331107c364314e958c",
-"index.html": "385c8340c3bb9fbac4eebd738502341d",
-"/": "385c8340c3bb9fbac4eebd738502341d",
+"index.html": "b8307169b94c6ae0657d4a230c73c80e",
+"/": "b8307169b94c6ae0657d4a230c73c80e",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin.json": "8de21b243b168daae31a46364ac1a0a7",
 "assets/assets/models/peugeot.glb": "79ff651212182287f142f0148e59391f",
@@ -34,9 +34,9 @@ const RESOURCES = {"flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "canvaskit/canvaskit.js.symbols": "bdcd3835edf8586b6d6edfce8749fb77",
 "canvaskit/skwasm.js.symbols": "e72c79950c8a8483d826a7f0560573a1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "fa1b35561f17c7de70cd0527b3ed5268",
+"flutter_bootstrap.js": "2833fd9ca8726b66432d4e2cbaad4d45",
 "version.json": "28d5d65d720d35cd7aae4535890ee52e",
-"main.dart.js": "d8f164fc083d7b8ed90dadcc9253178b"};
+"main.dart.js": "b3279c42727efa266355978f339482fe"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
